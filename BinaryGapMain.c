39f5c5ad2e22a9d890140b17519a10fbe169e239
NIcanor Kyamba <nicanorkyamba98@gmail.com>
2024-03-13 17:39:07 +0300

    Added more tests for binary gap

--- BinaryGapMain.c
+++ BinaryGapMain.c
@@ -1,8 +1,24 @@
 #include "BinaryGap.h"
 
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: void
+ */
+
+int main(void)
 {
-    int N = 1041;
-    printf("The length of the longest binary gap for %d is %d", N, solution(N));
-    return (0);
+	int N = 1041;
+	int M = 34;
+	int X = 10;
+	int G = -30;
+	int J = 200;
+
+	printf("The length of the longest binary gap for %d is %d", N, solution(N));
+	printf("\nThe length of the longest binary gap for %d is %d", M, solution(M));
+	printf("\nThe length of the longest binary gap for %d is %d", X, solution(X));
+	printf("\nThe length of the longest binary gap for %d is %d", G, solution(G));
+	printf("\nThe length of the longest binary gap for %d is %d", J, solution(J));
+
+	return (0);
 }
